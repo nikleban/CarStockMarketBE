094f67d509669c53d5bc9c6c49c6241effa2c5e6
nikleban <nikkatleban@gmail.com>
2026-03-14 18:52:06 +0100

    Add car seller api

--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import upload from "../middlewares/MulterUpload.js"
-import { createCarListing, getCarListings, getCarListing } from "../controllers/CarListing/CarListingController.js";
+import { createCarListing, getCarListings, getCarListing, getSellerData } from "../controllers/CarListing/CarListingController.js";
 
 const router = express.Router();
 
 router.post("/carListing", upload.array("photos", 10), createCarListing);
 router.get("/carListings", getCarListings);
 router.get("/carListing/:id", getCarListing);
+router.get("/carListing/sellerData/:id", getSellerData);
 
 export default router;
