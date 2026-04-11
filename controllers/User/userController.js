3afa1e6238073d064c07193e34f5be27bb8ed5dd
Nik Katanovic Leban <nikkatanovicleban@Niks-MacBook-Pro.local>
2026-04-11 17:18:13 +0200

    test pre commit

--- controllers/User/userController.js
+++ controllers/User/userController.js
@@ -1,23 +1,20 @@
-import bcrypt from "bcrypt";
-import User from "#/models/User.js";
-import {
-  MissingUserDataError,
-  UserAlreadyExistsError,
-} from "#/errors/index.js";
-import jwt from "jsonwebtoken";
-import CarListing from "../../models/CarListing.js";
-import { AppError } from "#/errors/index.js";
-import CarListingLike from "#/models/CarListingLike.js";
-import Brand from "#/models/Brand.js";
-import CarModel from "#/models/CarModel.js";
-import { literal } from "sequelize";
-import emailQueue from "#/modules/email/emailQueue.js";
+import bcrypt from 'bcrypt';
+import User from '#/models/User.js';
+import { MissingUserDataError, UserAlreadyExistsError, UserDoesntExist } from '#/errors/index.js';
+import jwt from 'jsonwebtoken';
+import CarListing from '../../models/CarListing.js';
+import { AppError } from '#/errors/index.js';
+import CarListingLike from '#/models/CarListingLike.js';
+import Brand from '#/models/Brand.js';
+import CarModel from '#/models/CarModel.js';
+import { literal } from 'sequelize';
+import emailQueue from '#/modules/email/emailQueue.js';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export const registerUser = async (req, res, next) => {
   try {
-    console.log("what")
+    console.log('what');
     const { firstName, lastName, email, telephone, password } = req.body;
 
     if (!firstName || !lastName || !email || !telephone || !password) {
@@ -40,16 +37,16 @@ export const registerUser = async (req, res, next) => {
     });
 
     const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
-      expiresIn: "7d",
+      expiresIn: '7d',
     });
 
-    res.cookie("token", token, {
+    res.cookie('token', token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     return res.status(201).json({
-      message: "User registered successfully",
+      message: 'User registered successfully',
       user: {
         id: user.id,
         firstName: user.firstName,
@@ -73,28 +70,28 @@ export const loginUser = async (req, res, next) => {
 
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      throw new AppError("Invalid email or password", 401);
+      throw new AppError('Invalid email or password', 401);
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      throw new AppError("Invalid email or password", 401);
+      throw new AppError('Invalid email or password', 401);
     }
 
     const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
-      expiresIn: "7d",
+      expiresIn: '7d',
     });
 
-    res.cookie("token", token, {
+    res.cookie('token', token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-    
-    await emailQueue.add("login", { type: "login", to: user.email, userName: user.firstName });
+
+    await emailQueue.add('login', { type: 'login', to: user.email, userName: user.firstName });
 
     return res.status(201).json({
-      message: "User loged in successfully",
+      message: 'User loged in successfully',
       user: {
         id: user.id,
         firstName: user.firstName,
@@ -114,7 +111,7 @@ export const getUserWithToken = async (req, res) => {
   const user = await User.findOne({ where: { email: userEmail } });
 
   return res.status(201).json({
-    message: "User loged in successfully",
+    message: 'User loged in successfully',
     user: {
       id: user.id,
       firstName: user.firstName,
@@ -126,8 +123,8 @@ export const getUserWithToken = async (req, res) => {
 };
 
 export const logoutUser = async (req, res) => {
-  res.clearCookie("token");
-  res.json({ message: "User logged out successfully" });
+  res.clearCookie('token');
+  res.json({ message: 'User logged out successfully' });
 };
 
 export const getUser = async (req, res, next) => {
@@ -140,36 +137,36 @@ export const getUser = async (req, res, next) => {
       throw UserDoesntExist();
     }
 
-    const carListingsMade = await CarListing.count({ where: { userId: userId } })
+    const carListingsMade = await CarListing.count({ where: { userId: userId } });
     const userActiveYear = user.createdAt.getFullYear();
 
     if (userId !== currentUserId) {
       //return limited data, private data stays
     }
-    
-    const totalLikes = await CarListingLike.count({where: { userId: user.id }});
-    
+
+    const totalLikes = await CarListingLike.count({ where: { userId: user.id } });
+
     return res.status(201).json({
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        telephone: user.telephone,
-        soldCars: user.soldCars,
-        activeListings: carListingsMade,
-        activeSinceYear: userActiveYear,
-        totalLikes: totalLikes,
-      });
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      telephone: user.telephone,
+      soldCars: user.soldCars,
+      activeListings: carListingsMade,
+      activeSinceYear: userActiveYear,
+      totalLikes: totalLikes,
+    });
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const getUserCarListings = async (req, res, next) => {
   const { id: userId } = req.params;
   try {
     const carListings = await CarListing.findAll({
-      where: {userId: userId},
+      where: { userId: userId },
       attributes: {
         include: [
           [
@@ -178,26 +175,30 @@ export const getUserCarListings = async (req, res, next) => {
               FROM "CarListingLikes" AS cl
               WHERE cl."carListingId" = "CarListing"."id"
             )`),
-            "likes"
-          ]
+            'likes',
+          ],
         ],
       },
-      include: [{
+      include: [
+        {
           model: CarModel,
-          attributes: ["name"],
-          include: [{
+          attributes: ['name'],
+          include: [
+            {
               model: Brand,
-              attributes: ["name"]
-          }],
-      },]
-    })
+              attributes: ['name'],
+            },
+          ],
+        },
+      ],
+    });
 
     const formatted = carListings.map((listing) => {
       const json = listing.toJSON();
       return {
         id: listing.id,
         brand: json.CarModel.Brand.name,
-        model: json.CarModel.name,  
+        model: json.CarModel.name,
         createdAt: listing.createdAt,
         description: listing.description,
         fuel: listing.fuel,
@@ -210,38 +211,42 @@ export const getUserCarListings = async (req, res, next) => {
         registrationYear: listing.registrationYear,
         likes: json.likes,
       };
-    })
+    });
 
     return res.status(201).json(formatted);
   } catch (error) {
     console.log(error);
     next(error);
   }
-}
+};
 
 export const getUserLikedCarListings = async (req, res, next) => {
   const { id: userId } = req.params;
   try {
     const likedCarListingIds = await CarListingLike.findAll({
-        where: { userId },
-        attributes: ['carListingId'],
-        raw: true,
-      });
+      where: { userId },
+      attributes: ['carListingId'],
+      raw: true,
+    });
 
     const ids = likedCarListingIds.map((row) => row.carListingId);
-    
+
     const carListings = await CarListing.findAll({
       where: {
         id: ids,
       },
-      include: [{
+      include: [
+        {
           model: CarModel,
-          attributes: ["name"],
-          include: [{
+          attributes: ['name'],
+          include: [
+            {
               model: Brand,
-              attributes: ["name"]
-          }],
-      },]
+              attributes: ['name'],
+            },
+          ],
+        },
+      ],
     });
 
     const formatted = carListings.map((listing) => {
@@ -249,7 +254,7 @@ export const getUserLikedCarListings = async (req, res, next) => {
       return {
         id: listing.id,
         brand: json.CarModel.Brand.name,
-        model: json.CarModel.name,  
+        model: json.CarModel.name,
         createdAt: listing.createdAt,
         description: listing.description,
         fuel: listing.fuel,
@@ -262,11 +267,11 @@ export const getUserLikedCarListings = async (req, res, next) => {
         registrationYear: listing.registrationYear,
         liked: true,
       };
-    })
+    });
 
     return res.status(201).json(formatted);
   } catch (error) {
     console.log(error);
     next(error);
   }
-}
+};
